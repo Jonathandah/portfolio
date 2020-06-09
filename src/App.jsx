525dd94ac8f70b8b66bd2b7df11e03759ff74821
unknown <jmd119988@gmail.com>
2020-06-10 00:26:12 +0200

    fixed styling and added icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.sass";
-import Home from "./Views/Home/Home"
-
+import Home from "./Views/Home/Home";
+import "@fortawesome/fontawesome-free";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App__header"></header>
+
         <main className="App__main">
-          <Route exact path="/" component={Home}/>
+          <aside className="App__sidebar">
+            <span class="material-icons">home</span>
+            <span class="material-icons">face</span>
+            <span class="material-icons">work</span>
+            <span class="material-icons">alternate_email</span>
+          </aside>
+          <Route exact path="/" component={Home} />
         </main>
-        {/* <footer className="App__footer"></footer> */}
+        <footer className="App__footer"></footer>
       </div>
     </Router>
   );
