1fb29a4c644cbe3036da35a1b3c0cab75265588b
Jonathan Meeprong Dahnberg <Jonathan.Dahnberg@cybercom.com>
2020-06-11 16:23:47 +0200

    removed bezels and new padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.sass";
-import Home from "./Views/Home/Home";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
- 
-library.add(fab)
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.sass';
+import Home from './Views/Home/Home';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faAlignRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fab);
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App__header"></header>
-        <aside className="App__sidebar">
-          <span className="material-icons">home</span>
+      <div className='App'>
+        <header className='App__header'></header>
+        <aside className='App__sidebar'>
+          <div className='sidebar__box'>
+            <FontAwesomeIcon className='box__icon' icon={faAlignRight} />
+            <p className='box__text'>Menu</p>
+          </div>
+          {/* <span className="material-icons">home</span>
           <span className="material-icons">face</span>
           <span className="material-icons">work</span>
-          <span className="material-icons">alternate_email</span>
+          <span className="material-icons">alternate_email</span> */}
         </aside>
-        <main className="App__main">
-          <Route exact path="/" component={Home} />
+        <main className='App__main'>
+          <Route exact path='/' component={Home} />
         </main>
-        <footer className="App__footer"></footer>
+        <footer className='App__footer'></footer>
       </div>
     </Router>
   );
