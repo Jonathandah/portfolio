9a3031d4cd4467ef451664eb07ec8415d81d8dd5
unknown <jmd119988@gmail.com>
2020-06-29 15:56:40 +0200

    added more styling and fixed grid overflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.sass';
-import Home from './Views/Home/Home';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faAlignRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.sass";
+import Home from "./Views/Home/Home";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faAlignRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 library.add(fab);
 
 function App() {
+  const [sidebar, updateSidebar] = useState(false);
+
+    function openSidebar(e) {
+      console.log(document.querySelector(".App__sidebar").classList);
+      if (!sidebar) {
+        document.querySelector(".App__sidebar").style.width = "40vw"
+        document.querySelector(".sidebar__content").style.width ="40vw"
+        updateSidebar(true);
+      } else {
+        document.querySelector(".App__sidebar").style.width = "60px"
+        document.querySelector(".sidebar__content").style.width ="0vw"
+        updateSidebar(false);
+      }
+    }
   return (
     <Router>
-      <div className='App'>
-        <header className='App__header'></header>
-        <aside className='App__sidebar'>
-          <div className='sidebar__box'>
-            <FontAwesomeIcon className='box__icon' icon={faAlignRight} />
-            <p className='box__text'>Menu</p>
+      <div className="App">
+        <header className="App__header"></header>
+        <aside className="App__sidebar">
+        {sidebar && <div className="sidebar__shading" />}
+        <div className="sidebar__content"> </div>
+
+       
+          <div className="sidebar__box" onClick={(e) => openSidebar(e)}>
+            <FontAwesomeIcon className="box__icon" icon={faAlignRight} />
+            <p className="box__text">Menu</p>
           </div>
-          {/* 
-          <span className="material-icons">home</span>
-          <span className="material-icons">face</span>
-          <span className="material-icons">work</span>
-          <span className="material-icons">alternate_email</span> */}
         </aside>
-        <main className='App__main'>
-          <Route exact path='/' component={Home} />
+     
+        <main className="App__main">
+   
+          <Route exact path="/" component={Home} />
         </main>
-        <footer className='App__footer'></footer>
+        <footer className="App__footer"></footer>
       </div>
     </Router>
   );
--- src/Views/Home/components/Contact/Contact.jsx
+++ src/Views/Home/components/Contact/Contact.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import './Contact.sass';
-import { Mail, GitHub, Linkedin } from 'react-feather';
+import React from "react";
+import "./Contact.sass";
+import { Mail, GitHub, Linkedin } from "react-feather";
 
 function Contact(props) {
   return (
-    <section className='Contact'>
-      <div className='Contact__container'>
-        <div className='container__media'>
-          <div className='media__title'>
-            <p className='title__question'>Want to know more?</p>
-            <h1 className='title__text'>Get in touch with me on,</h1>
+    <section className="Contact">
+      <div className="Contact__container">
+        <div className="container__media">
+          <div className="media__title">
+            <p className="title__question">Want to know more?</p>
+            <h1 className="title__text">Get in touch with me on,</h1>
           </div>
-          <ul className='media__list'>
-            <li className='list__item'>
-              <Mail className="item__icon"/>
-              <p className='item__text'>Jonathan.dahnberg1@gmail.com</p>
+          <ul className="media__list">
+            <li className="list__item">
+              <a href="mailto: jonathan.dahnberg1@gmail.com">
+                <Mail className="item__icon" />
+                <p className="item__text">Jonathan.dahnberg1@gmail.com</p>
+              </a>
             </li>
-            <li className='list__item'>
-              <Linkedin className="item__icon" />
-              <p className='item__text'>Jonathan Dahnberg</p>
+            <li className="list__item">
+              <a href="https://www.linkedin.com/in/jonathan-meeprong-dahnberg-5289ba174/"
+              target="_blank"
+              rel="noopener noreferrer">
+                <Linkedin className="item__icon" />
+                <p className="item__text">Jonathan Dahnberg</p>
+              </a>
             </li>
-            <li className='list__item'>
-              <GitHub className="item__icon"/>
-              <p className='item__text'>Jonathandah</p>
+            <li className="list__item">
+              <a href="https://github.com/Jonathandah"
+              target="_blank"
+              rel="noopener noreferrer">
+                <GitHub className="item__icon" />
+                <p className="item__text">Jonathandah</p>
+              </a>
             </li>
           </ul>
         </div>
